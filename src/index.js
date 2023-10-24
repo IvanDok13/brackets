0bb131fe437cca329539738481c8af46309fbde0
Ivan Klochkov <klochkov.ivan.sergeevich@gmail.com>
2023-10-24 22:27:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, brackets) {
+  const pairs = new Map(brackets);
+  const stack = [];
+  for (const char of str) {
+    pairs.get(stack.at(-1)) == char? stack.pop() : stack.push(char);
+  }
+  return stack.length === 0;
 }
